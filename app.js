3512f34d4097b441a80ce3321bcf2aa2856bcea9
bluffitee <69552200+bluffitee@users.noreply.github.com>
2020-08-17 00:05:01 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const { MongoClient } = require("mongodb");
+const express = require("express");
+require("dotenv").config();
+let PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+const uri = process.env.MONGODB_URI;
+
+// const client = new MongoClient(
+// 	uri,
+// 	{ useNewUrlParser: true },
+// 	{ useUnifiedTopology: true }
+// );
+
+app.get("/api", (req, res) => {
+	MongoClient.connect(
+		uri,
+		{ useNewUrlParser: true },
+		{ useUnifiedTopology: true },
+		(err, db) => {
+			if (err) console.log(err);
+			var mdb = db.db("sample_analytics");
+			var mysort = { account_id: 1 };
+			mdb.collection("accounts")
+				.find()
+				.sort(mysort)
+				.toArray(function (err, result) {
+					if (err) console.log(err);
+					console.log(result);
+					res.json(result);
+					db.close();
+				});
+			// perform actions on the collection object
+			// const results = collection.find({}).toArray();
+			// console.log(collection);
+			// client.close();
+		}
+	);
+});
+
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+});
